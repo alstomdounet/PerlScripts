4c30e6b8d1ac40276abbbc56aeded876847cbff3
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-01-06 13:24:44 +0000

    Improved interface.

--- changeConfigSpec.bat
+++ changeConfigSpec.bat
@@ -45,35 +45,36 @@ DEBUG "Building graphical interface";
 my $mw = MainWindow->new(-title => "Interface change config-spec");
 my $balloon = $mw->Balloon();
 $mw->withdraw; # disable immediate display
-$mw->minsize(300,200);
-$mw->maxsize(300,200);
+$mw->minsize(640,480);
+$mw->maxsize(640,480);
 
-my $titlePanel = $mw->Frame() -> pack(-side => 'top', -fill => 'both', -expand => 1);
-my $listBoxesPanel = $mw->Frame() -> pack(-side => 'top', -fill => 'both', -expand => 1);
-#my $buttonsPanel = $mw->Frame() -> pack(-side => 'top', -fill => 'both', -expand => 1);
+my $titlePanel = $mw->Frame() -> pack(-side => 'top', -fill => 'x');
+my $mainPanel = $mw->Frame() -> pack(-side => 'top', -fill => 'both', -expand => 1);
+my $configSpecPanel = $mainPanel->Frame()->pack(-side => 'right', -fill => 'both', -expand => 1);
+my $buttonsPanel = $mw->Frame(-height => '30') -> pack(-side => 'bottom', -fill => 'both');
 
 $titlePanel->Label(-text => "Cette interface permet de modifier le fichier\nde configuration(config-spec) de Clearcase, afin de\nmodifier les élements affichés")->pack( -side => 'top', -fill => 'both', -expand => 1 );
 
-sub  processSelectedFile {
-	my $selection = shift;
-	DEBUG "Here is selected item";
-}
+my $cancelButton = $buttonsPanel->Button(-text => 'Quitter', -command => sub { exit(-1)}) -> pack(-side => 'left', -fill => 'both', -expand => 1);
+$buttonsPanel->Button(-text => 'Valider' , -command => sub { confirm()}) -> pack(-side => 'right', -fill => 'both', -expand => 1);
+
 
 #--------------------------------------------------------------------------
 # Tk::Tree Widget
 #--------------------------------------------------------------------------
-  my $jobstree = $listBoxesPanel->Scrolled(
+  my $jobstree = $mainPanel->Scrolled(
       'Tree',
       -background         => 'white',
+      -selectmode         => 'extended',
       -selectbackground   => 'LightGoldenrodYellow',
       -selectforeground   => 'RoyalBlue3',
       -highlightthickness => 0,
-      -font               => 'verdana 12',
+      -font               => 'verdana 10',
       -relief             => 'flat',
       -scrollbars         => 'osoe',
       -borderwidth        => 1,
-      -command          => \&processSelectedFile,
-   )->pack(-side => 'left', -fill => 'both', -expand => 1, -anchor => 'w');
+      -browsecmd         => \&processSelectedFile,
+   )->pack(-side => 'left', -fill => 'y',-anchor => 'w');
 
 #--------------------------------------------------------------------------
 # Tk::Tree Widget additional configurations
@@ -82,10 +83,12 @@ sub  processSelectedFile {
       -separator  => '/',
       -drawbranch => 'true',
       -indicator  => 'true',
-      -selectborderwidth => '4',
-      #-selectmode        => 'extended',
+      -selectborderwidth => '0',
+      -selectmode        => 'extended',
       -highlightcolor => 'red');
    $jobstree->focus();
+
+
 
 foreach my $node (@list) {
    my $node_name = (split('/', $node))[-1];
@@ -94,10 +97,29 @@ foreach my $node (@list) {
 }
 $jobstree->autosetmode();
 
-   
+sub  processSelectedFile {
+	my $selection = shift;
+	DEBUG "Here is selected item ($selection)";
+}   
+
 INFO "displaying graphical interface";
-$mw->Popup; # window appears screen-centered
+center($mw);
 MainLoop();
+
+sub center {
+  my $win = shift;
+
+  $win->withdraw;   # Hide the window while we move it about
+  $win->update;     # Make sure width and height are current
+
+  # Center window
+  my $xpos = int(($win->screenwidth  - $win->width ) / 2);
+  my $ypos = int(($win->screenheight - $win->height) / 2);
+  $win->geometry("+$xpos+$ypos");
+
+  $win->deiconify;  # Show the window again
+}
+
 
 sub createStructure {
 	my $directory  = shift;
