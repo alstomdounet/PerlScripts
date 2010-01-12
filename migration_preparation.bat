11c88ca484b148048fb973f40ef7b3ded55b1c07
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-01-12 19:22:45 +0000

    Completed script.

--- migration_preparation.bat
+++ migration_preparation.bat
@@ -10,480 +10,70 @@ use warnings;
 use Common;
 use Storable qw(store retrieve);
 use Data::Dumper;
+use File::Basename;
+use File::Copy;
+use File::Find;
+use ClearcaseMgt qw(checkoutElement isLatest setAttribute isPrivateElement checkinElement);
 
 my %Config = loadConfig("config.xml", ForceArray => qr/_list$/); # Loading / preprocessing of the configuration file
 
-# Preliminary checks
-ERROR "ControlBuild project was not found inside '$Config{project_params}->{folders}->{cb_project_folder}'" and exit unless -d $Config{project_params}->{folders}->{cb_project_folder}."\\Applications";
+my $srcDir = "./docs";
+my $path = 'D:\\clearcase_storage\\gmanciet_view\\PRIMA2\\ProjectSpecificDocs\\02_Requirements\\SyFRSCC\\TFE\\TRF';
+my $destFile = 'BAD0002243278 SyFRScc';
+my $initialVersion = 'A0';
+my $initialState = '10';
+my $ONLINE_MODE = 0;
 
-my @lines = putFileInMemory($Config{script_params}->{properties_file});
+my $result;
+find(sub { 
+INFO "Found match" and $result = $_ if /^$destFile.*\.doc$/; 
+ }, $path);
+$destFile = $result if $result;
 
-my $fbsTreePath = $Config{project_params}->{tree_view}->{fbs_path};
+LOGDIE "Path $path doesn't exists" unless -d $path;
+LOGDIE "Destination file $destFile doesn't exists" unless -f $path."\\".$destFile;
+#LOGDIE "File is not configuration management" if isPrivateElement($path."\\".$destFile);
+#LOGDIE "File is not latest revision." unless isLatest($path."\\".$destFile);
 
-cleanup("output");
-LOGDIE "Directory was not purged successfully" if -d "output";
-mkdir "output";
-LOGDIE "Destination directory \"output\" was not created successfully" unless -d "output";
+my @list = extract_list($srcDir);
 
-open MAINBACKUPFILE, ">output/allFunctions.backup.csv";
-binmode MAINBACKUPFILE;
-print MAINBACKUPFILE $lines[0];
-open LISTOFMIGRATEDFUNCTIONS, ">output/details.txt";
-open REMAININGFUNCTIONS, ">output/remainingFunctions.xml";
-my $MAINSCRIPTFILE;
-open $MAINSCRIPTFILE, ">output/allFunctions.bat";
-printScriptHeader($MAINSCRIPTFILE);
-my $OLDDIR = "$Config{project_params}->{folders}->{cb_project_folder}\\$Config{project_params}->{folders}->{fbs_folder}";
-my $message;
+setCCAttributes($path."\\".$destFile, $initialVersion, $initialState) if $ONLINE_MODE;
 
-my $selectedComponents = 0;
-my %remainingElements;
-my %componentsToBeMigrated;
-foreach my $element (@{$Config{function_list}}) {
-	my $finalDirectory = "output/".$element->{function_name};
-	my $functionName = $element->{function_name};
-	my $NEWDIR = $Config{project_params}->{folders}->{cb_project_folder}."\\Applications\\".$functionName."\\functional";
-
-	ERROR "Controlbuild application has to be created before executing this program\nFollow this rule:\n\t - Create A new application with ControlBuild called '$functionName'\n" unless -d "$Config{project_params}->{folders}->{cb_project_folder}\\Applications\\$functionName\\functional";
-	
-	mkdir "$finalDirectory";
-	LOGDIE "Destination directory \"$finalDirectory\" was not created successfully" unless -d "output";
-	
-	INFO "Processing function \"$functionName\"";
-	# Removing old structure if it exists
-
-	mkdir $finalDirectory;
-	LOGDIE "Destination directory \"$finalDirectory\" was not created successfully" unless -d $finalDirectory;
-	
-	my @foundComponents = filterFile(\@lines, "$finalDirectory/$functionName.backup.csv", $fbsTreePath, $functionName);
-	my @selectedComponents = selectComponents(@foundComponents);
-	
-	WARN "Function is skipped (nothing to migrate)" and next unless scalar(@selectedComponents);
-	
-	foreach my $component (@selectedComponents) {
-		ERROR "\"$component\" has already been selected by function \"$componentsToBeMigrated{$component}\"" if exists($componentsToBeMigrated{$component});
-		$componentsToBeMigrated{$component} = $functionName;
-	}
-	
-	$remainingElements{$functionName} = scalar(@selectedComponents);
-	
-	$selectedComponents += scalar(@selectedComponents);
-	INFO "$selectedComponents components ready to be migrated";
-	my $components = join(" ", @selectedComponents);
-	$message = <<EOF;
-
-INFO 'Checking prequisites for function \"$functionName\"';
-if (checkFunctionAvailability('$OLDDIR', qw($components))) {
-	#INFO 'Locking all components of \"$functionName\""';
-	#doLockRecursive('$OLDDIR', qw($components));
-
-	INFO 'Putting FBS in checkout state';
-	doCommand('cleartool co -c "Migration de la fonction $functionName" "$OLDDIR"');
-
-	INFO 'Putting function $functionName in checkout state';
-	doCommand('cleartool co -c "Migration de la fonction $functionName" "$NEWDIR"');
-
-EOF
-	printProtected ($MAINSCRIPTFILE, $message);
-	
-	createFilesStructure($functionName, \@selectedComponents, $finalDirectory, $MAINSCRIPTFILE, $NEWDIR);
-	
-	$message = <<EOF;
-	INFO 'Putting function $functionName in checkin state';
-	doCommand('cleartool ci -c "Migration de la fonction $functionName" "$NEWDIR"');
-
-	INFO 'Postprocessing of function \"$functionName\"(checkout, unlocking)';
-	doCheckoutRecursive('$NEWDIR', '..');
-	#doUnlockRecursive('$NEWDIR', '');
-
-	INFO 'Putting FBS in checkin state';
-	doCommand('cleartool ci -c "Migration de la fonction $functionName" "$OLDDIR"');
+foreach my $version (@list) {
+	my $file = $version;
+	$version =~ s/^(.*)\.doc$/$1/i;
+	INFO "Processing Version $version";
+	checkout($path."\\".$destFile) or LOGDIE 'Checkout was not performed correctly';
+	unlink($path."\\".$destFile) or LOGDIE 'Removal was not performed correctly';
+	copy($srcDir.''.$file, $path."\\".$destFile) or LOGDIE 'Copy was not performed correctly';
+	checkin($path."\\".$destFile) or LOGDIE 'Checkin was not performed correctly';
+	setCCAttributes($path."\\".$destFile, $version, 10) or LOGDIE 'Attributes were not set correctly';
 }
 
-EOF
-	printProtected ($MAINSCRIPTFILE, $message);
-	
-}
-
-my $key;
-foreach $key (sort {$remainingElements{$a} <=> $remainingElements{$b} } (keys(%remainingElements))) {
-   print LISTOFMIGRATEDFUNCTIONS "$key ($remainingElements{$key} components)\n";
-   
-   	print REMAININGFUNCTIONS <<EOF
-	<function_list>
-		<function_name>$key</function_name><!-- Name of the function you want to move ($remainingElements{$key} components) -->
-	</function_list>
-EOF
-}
-
-
-
-
-
-printScriptFooter($MAINSCRIPTFILE);
-close MAINBACKUPFILE;
-close LISTOFMIGRATEDFUNCTIONS;
-close REMAININGFUNCTIONS;
-close $MAINSCRIPTFILE;
-
-exit;
-
-sub filterFile {
-	my $lines = shift;
-	my $outputFile = shift;
-	my $baseTreePath = shift;
-	my $functionName = shift;
-	
-	my %foundComponents;
-	my @lines = @$lines;
-	
-	 $| = 1;
-	my $matches = 0;
-	open BACKUP, ">$outputFile" or LOGDIE "Not possible to write in file \"$outputFile\" : $!"; 
-	binmode BACKUP;
-	print BACKUP shift(@lines);
-	
-	my $suggestedPath = undef;
-	
-	foreach my $line (@lines) {
-		if ($line->{component_name} =~ /^$functionName$/) {
-			$suggestedPath = $line->{path};
-			$suggestedPath =~ s/^$baseTreePath//;
-			DEBUG "Found automatic path \"$suggestedPath\"";
-			last;
-		}
-	}
-	
-	unless ($suggestedPath) {
-		ERROR "No matches were found for function \"$functionName\". It has to be an existing function." and return () unless -d $OLDDIR."\\$functionName";
-		WARN "Only one component has been found. It is probably an empty component.";
-		return ($functionName);
-	}
-
-	my $matchingPath = "^".$baseTreePath.$suggestedPath;
-	DEBUG "Selected path  : '$matchingPath'";	
-	
-	foreach my $line (@lines) {
-		if($line->{path} =~ /$matchingPath/) 
-		{
-			print BACKUP $line->{all};
-			print MAINBACKUPFILE $line->{all};
-			my $componentName = $line->{component_name};
-			$componentName =~ s/_$line->{variable}//;
-			$foundComponents{$componentName}++;
-			
-			$matches++;
-		}
-	}
-	
-	close BACKUP;
-
-	INFO "$matches lines were written for backup";
-		
-	my @foundComponents = keys(%foundComponents);
-	INFO scalar(@foundComponents)." components found";
-	return @foundComponents;
-}
-
-sub selectComponents {
-	my @selectedComponents = ();
-	foreach my $component (@_) {
-		LOGDIE "Empty component found" if $component eq "";
-		my $componentPath = "$Config{project_params}->{folders}->{cb_project_folder}\\$Config{project_params}->{folders}->{fbs_folder}\\$component";
-		push (@selectedComponents, $component) if -d $componentPath;
-	}
-
-	INFO scalar(@selectedComponents)." components selected";
-	return @selectedComponents;
-}
-
-sub cleanup {
+# Extract list
+sub extract_list {
     my $dir = shift;
 	local *DIR;
 
 	return unless -d $dir;
 	opendir DIR, $dir or LOGDIE "opendir failed for \"$dir\" : $!";
+	my @results ;
 	for (readdir DIR) {
 	        next if /^\.{1,2}$/;
-	        my $path = "$dir/$_";
-		unlink $path if -f $path;
-		cleanup($path) if -d $path;
-		LOGDIE "It was not possible to remove element \"$path\" : $!" if -e $path;
+			WARN "$_ has not correct syntax. It will be ignored" and next unless(/^[A-Z]\d\.doc$/);
+	        push (@results , $_);
 	}
 	closedir DIR;
-	rmdir $dir or LOGDIE "Deletion of \"$dir\" is not possible  : $!";
+	return sort @results;
 }
 
-sub putFileInMemory {
-	my $inputFile = shift;
+sub setCCAttributes {
+	my $document = shift;
+	my $version = shift;
+	my $state = shift;
 	
-	my @lines;
-	if(-r "db.tmp") {
-		DEBUG "Beginning reading of database";
-		my $lines = retrieve("db.tmp");
-		@lines = @$lines;
-		DEBUG "Finished reading of database";
-	}
-	else {
-		open FILE, $inputFile or die $!;
-		binmode FILE;
-		my $header = <FILE>;
-		push(@lines, $header);
-		my $linesProcessed = 0;
-		while (my $line = <FILE>) {
-			my %line;
-			my @items = split (/;/, $line);
-			my $path = $items[3];
-			
-			my $offsetDuToCommas = 0;
-			while(defined($items[8+$offsetDuToCommas]) and !($items[8+$offsetDuToCommas] =~ /^(-1|\d+)$/)) { $offsetDuToCommas++; }
-			LOGDIE "Line $linesProcessed -> No match found:\n$line" unless defined($items[8+$offsetDuToCommas]);
-			WARN "Line $linesProcessed -> Offset inserted : $offsetDuToCommas (stopped at ".$items[8+$offsetDuToCommas].")" if $offsetDuToCommas > 0;
-			
-			my $component_name = $items[11+$offsetDuToCommas];
-			$line{path} = $items[3];
-			$line{component_name} = substr($component_name, 0, -(length($items[0]) + 1));
-			
-			LOGDIE "Empty component name. parameters are '$component_name' and '$items[0]' and '".(-(length($items[0]) + 1))."'" unless $line{component_name};
-			
-			$line{all} = $line;
-			$line{variable} = $items[0];
-			push(@lines, \%line);
-			$linesProcessed++;
-			DEBUG "$linesProcessed lines processed" if $linesProcessed % 10000 == 0;
-		}
-		close FILE;
-	
-		store(\@lines, "db.tmp");
-	}
-	
-	INFO "Script has read ".scalar(@lines)." lines of properties";
-	return @lines;
-}
-
-sub write_file {
-	my $file = shift;
-	my $text = shift;
-	
-	open WRITE_FILE, ">$file" or die "Script was not able to write '$file' : $!";
-	print WRITE_FILE $text;
-	close WRITE_FILE;
-}
-
-sub printProtected {
-	my $handler  = shift;
-	my $message = shift;
-	
-	$message =~ s/\\/\\\\/g;
-	print $handler $message;
-	
-}
-
-sub printScriptHeader {
-	my $handler = shift;
-	
-	print $handler <<'EOF';
-@rem = ' PERL for Windows NT - ccperl must be in search path
-@echo off
-ccperl %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
-goto endofperl
-@rem ';
-
-use lib qw(../lib);
-use strict;
-use warnings;
-use Common;
-use Time::HiRes qw(gettimeofday tv_interval);
-use File::Find;
-use Term::ReadKey;
-
-print <<EOM;
-WARNING : PLEASE CHECK FOLLOWING POINTS:
-----------------------------------------
-  - CHECK IF THIS BATCH FILE IS CORRECT BEFORE EXECUTING IT :
-  - HAVE YOU EXECUTED THIS SCRIPT WITH AN UPDATED CSV FILE
-  - CHECK COMPONENTS WHICH WILL BE MOVED
-  - FUNCTION WHICH WILL BE MOVED IS IN A STABLE STATE
-----------------------------------------
-IF YOU HAVE UNDERSTOOD THIS MESSAGE, YOU CAN PROCEED...
-----------------------------------------
-EOM
-<>;
-
-EOF
-}
-
-sub printScriptFooter {
-	my $handler = shift;
-	
-	print $handler <<'EOF';
-print <<EOM;
-----------------------------------------
-Here are all remaining operations:
-  1 / Save all components which were moved;
-  2 / Open all MACS which has these components instanciated, and change model. BE VERY CAREFULL TO SELECT THE RIGHT MODEL BEFORE CHANGING.
-  3 / Save all the modified MACS;
-  4 / For safety, reimport backup advanced properties into top-level tree;
-  5 / For safety, run a coherency test. No new error messages should have appeared.
-----------------------------------------
-EOM
-<>;
-
-sub doMove {
-	my $oldDir = shift;
-	my $newDir = shift;
-	doCommand("cleartool move \"$oldDir\" \"$newDir\"");
-}
-
-sub doCheckoutRecursive {
-	my $baseDir = shift;
-	my @directories = ();
-	foreach my $dir (@_) {
-		push(@directories, File::Spec->canonpath($baseDir."\\".$dir));
-	}
-	my $t1 = [gettimeofday];
-	find(\&checkoutFile, @directories);
-	DEBUG "Checkout has taken ".tv_interval ( $t1, [gettimeofday] )." seconds";
-}
-
-sub checkFunctionAvailability {
-	my $baseDir = shift;
-	my @directories = @_;
-	my $files;
-RETRY:
-	$files = '';
-	foreach my $dir (@directories) {
-		my $path = File::Spec->canonpath($baseDir."\\".$dir);
-		$files .= `cleartool lsco -r "$path" 2>&1`;
-	}
-
-	unless ($files =~ /^\s*$/) {
-		ERROR "Below are all errors found:\n$files";
-		print "Something strange happened. Do you want to ignore (i), skip (s) or retry (r)? ";
-		ReadMode('cbreak');
-		my $key = ReadKey(0);
-		ReadMode('normal');
-		INFO "Function is skipped" and return 0 if $key eq 's' or $key eq 'S';
-		print "\rRetrying                                                           \r" and goto RETRY unless $key eq 'i' or $key eq 'I';
-		print "\r                                                                    \r";
-	}
-	return 1;
-}
-
-sub doLockRecursive {
-	my $baseDir = shift;
-	my @directories = ();
-	foreach my $dir (@_) {
-		push(@directories, File::Spec->canonpath($baseDir."\\".$dir));
-	}
-	my $t1 = [gettimeofday];
-	find(\&lockFile, @directories);
-	INFO "Locking has taken ".tv_interval ( $t1, [gettimeofday] )." seconds";
-}
-
-sub doUnlockRecursive {
-	my $baseDir = shift;
-	my @directories = ();
-	foreach my $dir (@_) {
-		push(@directories, File::Spec->canonpath($baseDir."\\".$dir));
-	}
-	my $t1 = [gettimeofday];
-	find(\&lockFile, @directories);
-	INFO "Locking has taken ".tv_interval ( $t1, [gettimeofday] )." seconds";
-}
-
-sub unlockFile {
-	my $file = $File::Find::name;
-	$file =~ s/\//\\/g;
-	doCommand("cleartool unlock -c \"Migration des fonctions. Demander a gmanciet en cas de problemes.\" \"$file\"", 0, 1);
-}
-
-sub lockFile {
-	my $file = $File::Find::name;
-	$file =~ s/\//\\/g;
-	doCommand("cleartool lock -c \"Migration des fonctions. Demander a gmanciet en cas de problemes.\" -nuser gmanciet \"$file\"", 0, 1);
-}
-
-sub checkoutFile {
-	my $file = $File::Find::name;
-	$file =~ s/\//\\/g;
-	doCommand("cleartool co -c \"Migration de la fonction.\" \"$file\"", 0, 1);
-}
-
-sub doCommand {
-	my $command = shift;
-	my $skipCheckpoint = shift;
-	my $skipRecording = shift;
-	
-RETRY:
-	my $t0 = [gettimeofday];
-	DEBUG "Command entered : >>>$command<<<" unless $skipRecording;
-	my $result = `$command 2>&1 1>NUL`;
-	DEBUG "Command has taken ".tv_interval ( $t0, [gettimeofday] )." seconds" unless $skipRecording;
-	
-	my $returnString = 'UNKNOWN';
-	my $message = '';
-	if ($result eq '') {
-		$returnString = "OK";
-	} elsif ($result =~ /^cleartool: Error:\s*(.*)/) {
-		$message = $1;
-		$returnString = "ERROR";
-	} elsif ($result =~ /^cleartool: Warning:\s*(.*)/) {
-		$message = $1;
-		$returnString = "WARNING";
-	} 
-	
-	if($returnString ne 'OK') {
-		WARN "Command entered was : >>>$command<<<";
-		if($returnString eq 'WARNING') {
-			WARN "Warning returned by command: $message";
-		} elsif ($returnString eq 'ERROR') {
-			ERROR "Error returned by command: $message";
-		} else {
-			ERROR "Unknown event : >>>$result<<<";
-		}
-		
-		print "Something strange happened. Do you want to ignore (i) or retry (r)? ";
-		ReadMode('cbreak');
-		my $key = ReadKey(0);
-		ReadMode('normal');
-		#print "\n".ord($key)."\n";
-		print "\rRetrying                                                           \r" and goto RETRY unless $key eq 'i' or $key eq 'I';
-		print "\r                                                                    \r";
-		return $returnString;
-	}
-	return $returnString;
-}
-
-
-__END__
-:endofperl
-pause
-EOF
-}
-
-sub createFilesStructure {
-	my $functionName = shift;
-	my $selectedComponents = shift;
-	my $directory = shift;
-	my $MAINSCRIPTFILE = shift;
-	my $NEWDIR = shift;
-	
-	my $i = 0;
-	my $total = scalar(@$selectedComponents);
-	foreach my $component (@$selectedComponents) {
-		$i++;
-		
-		$message = <<EOF;
-		INFO 'Processing "$component"';
-		doMove('$OLDDIR\\$component', '$NEWDIR\\$component');
-		
-EOF
-		printProtected ($MAINSCRIPTFILE, $message);
-	}
-
-	close FILE;
-	close BACKUP;
+	my $result = setAttribute($document, 'Version',  $version);
+	return $result and setAttribute($document, 'State', $state);
 }
 
 
