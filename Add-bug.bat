9b64458e58c18083c57eacd5d9a8ec6e2ee0bce3
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-01-12 07:56:24 +0000

    Added searchable text-boxes

--- Add-bug.bat
+++ Add-bug.bat
@@ -745,6 +745,93 @@ sub validate {
 	
 }
 
+sub addSearchableListBox {
+	my $parentElement = shift;
+	my $labelName = shift;
+	my $CQ_Field = shift;
+	my $necessityText = shift;
+	my $labelDescription = shift;
+	my $completeList = shift;
+
+	my %item;
+	my @list;
+
+	my $oldValue = $bugDescription{$CQ_Field};
+	$item{searchActivated} = 0;
+	$item{selectedList} = \@list;
+	$item{selection} = \$bugDescription{$CQ_Field};
+	$item{mainFrame} = $mw->Frame()->pack(-side => 'top', -fill => 'x');
+	$item{mainFrame}->Label(-text => $labelName, -width => 15 )->pack(-side => 'left');
+	$item{searchButton} = $item{mainFrame}->Button(-text => 'Search', -command => sub { manageSearchBox(\%item) })->pack( -side => 'right' );
+	$item{listbox} = $item{mainFrame}->JComboBox(-choices => $item{selectedList}, -textvariable => $item{selection})->pack(-fill => 'x', -side => 'left', -expand => 1);
+	$item{searchFrame} = $item{mainFrame}->Frame();
+	$item{searchFrame}->Label(-textvariable => \$item{searchText})->pack(-side => 'left');
+	$item{searchFrame}->Entry(-validate => 'all', -textvariable => \$item{search}, -width => 15, -validatecommand => sub { my $search = shift; search(\%item, $search); return 1; } )->pack(-side => 'right');
+
+	changeList(\%item, $completeList, $oldValue);
+	$balloon->attach($item{listbox}, -msg => "<$necessityText> $labelDescription");
+	push(@mandatoryFields, {Text => $labelName, CQ_Field => $CQ_Field});
+
+	return %item;
+}
+
+sub changeList {
+	my $item = shift;
+	my $completeList = shift;
+	my $selection = shift;
+	
+	$item->{completeList} = $completeList;
+
+	my @list = sort keys %$completeList;
+	@{$item->{selectedList}} = @list;
+	
+	DEBUG "Trying to set default value \"$selection\"" and $item->{listbox}->setSelected($selection) if $selection;
+}
+
+sub manageSearchBox {
+	my $searchListbox = shift;
+	
+	$searchListbox->{searchActivated} = ($searchListbox->{searchActivated}+1)%2;
+	if($searchListbox->{searchActivated}) {
+		DEBUG "Search activated";
+		$searchListbox->{searchButton}->configure(-text => 'X');
+		$searchListbox->{searchFrame}->pack(-fill => 'x', -side => 'right', -anchor => 'center');
+		$balloon->attach($searchListbox->{searchButton}, -msg => 'Cancel search');
+	}
+	else {
+		DEBUG "Search deactivated";
+		$searchListbox->{search} = '';
+		$searchListbox->{searchButton}->configure(-text => 'Search');
+		$searchListbox->{searchFrame}->packForget();
+		$balloon->attach($searchListbox->{searchButton}, -msg => 'Perform a search on left list');
+	}
+}
+
+sub search {	
+	my $searchListbox = shift;
+	my $search = shift;
+	
+	DEBUG "Search request is : \"$search\"";
+	my @tmpList;
+	my %completeList = %{$searchListbox->{completeList}};
+	my @resultsText = ("Hereafter are results remainings:");
+	my $old_selection = ${$searchListbox->{selection}};
+	foreach my $item (keys %completeList) {
+		next unless ($item =~ /$search/i or $completeList{$item}{comment} =~ /$search/i);
+		push (@tmpList, $item);
+		push (@resultsText, " => $item --- $completeList{$item}{comment}");
+	}
+	my $nbrOfResults = scalar(@tmpList);
+	@{$searchListbox->{selectedList}} = sort @tmpList;
+	${$searchListbox->{selection}} = $old_selection if $old_selection;
+	${$searchListbox->{selection}} = $tmpList[0] if $nbrOfResults == 1;
+
+	$balloon->attach($searchListbox->{searchFrame}, -msg => join("\n", @resultsText));
+	$searchListbox->{listbox}->configure(-state => $nbrOfResults ? 'normal' : 'disabled');
+	$searchListbox->{searchText} = ($nbrOfResults ? ($nbrOfResults == 1 ? "1 result" : $nbrOfResults.' results' ) : 'No results');
+	return 1;
+}
+
 ##########################################################
 # Database related functions
 ##########################################################
