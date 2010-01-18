f223f8662c0400ca1b4be547b99c4674590e47e9
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-01-18 17:24:17 +0000

    Built basic interface.

--- UI.bat
+++ UI.bat
@@ -16,6 +16,23 @@ use GraphicalCommon;
 ############################################################################################
 # 
 ############################################################################################
+WARN "Missing dynamic retrieval of bugs";
+my @listOfBugs = qw(atvcm000125654 atvcm000125644 atvcm000125694);
+my %CqFieldsDesc;
+my $CqDatabase = 'ClearquestImage.db';
+my $bugsDatabase = 'bugsDatabase.db';
+if (-r $CqDatabase) {
+	my $storedData = retrieve($CqDatabase);
+	%CqFieldsDesc = %$storedData;
+}
+else { LOGDIE "Not valid database"; }
+my $processedCR;
+my $contentFrame;
+
+
+############################################################################################
+# 
+############################################################################################
 use Tk;
 use Tk::JComboBox;
 use Tk::Balloon;
@@ -24,19 +41,8 @@ use Tk::Pane;
 ############################################################################################
 # 
 ############################################################################################
-my %bugDescription;
-my %CqFieldsDesc;
-my $syncNeeded = 0;
-my $CqDatabase = 'ClearquestImage.db';
-my $bugsDatabase = 'bugsDatabase.db';
-my (@listSubSystems, @mandatoryFields);
+#my %bugDescription;
 
-
-if (-r $CqDatabase) {
-	my $storedData = retrieve($CqDatabase);
-	%CqFieldsDesc = %$storedData;
-}
-else { $syncNeeded = 1; }
 
 ##########################################
 # Building graphical interface
@@ -56,24 +62,15 @@ my $mw = MainWindow->new(-title => "Interface to distribute bugs");
 $mw->withdraw; # disable immediate display
 $mw->minsize(640,480);
 
-my $lastSelectedSubsystem = '';
-
-
 # SyFRSCC, Component, Type, ?headline?, CCB comment, Description / context, Proposed changes, Analysis cost+System+Hardware+Software+Validation+ Inmpacted items + Analyst + Work in progress
-
-my $parentFrame = $mw->Frame()->pack();
-
-my $notebook = $mw->NoteBook()->pack( -fill=>'both', -expand=>1 );
-
-my %tab;
-$tab{ID} = "atvcm000122211";
-buildTab($notebook,\%tab);
+addSearchableListBox($mw, 'Selected CR', 'parent_id', undef, undef,\@listOfBugs);
 
 # Building buttons
 my $bottomPanel = $mw->Frame()->pack(-side => 'bottom', -fill => 'x');
 $bottomPanel->Button(-text => 'Cancel', ,-font => 'arial 9', -command => [ \&cancel, $mw], -height => 2, -width => 10) -> pack(-side => 'left');
 my $buttonSwitch = $bottomPanel->Button(-text => "Validate",-font => 'arial 9', -command => [ \&switchActions], -height => 2, -width => 10) -> pack(-side => 'right');
 
+loadCR('testID');
 
 $mw->Popup; # window appears screen-centered
 MainLoop();
@@ -81,19 +78,56 @@ MainLoop();
 ############################################################################################
 # 
 ############################################################################################
+sub loadCR {
+	my $id = shift;
+	use Tk::ROText;
+	
+	$processedCR = undef;
+	
+	use Fields;
+	
+	DEBUG "Destroying content frame" and $contentFrame->destroy() if $contentFrame;
+	
+	$contentFrame = $mw->Frame()->pack( -fill=>'both', -expand => 1);
+
+	my $parentFrame = $contentFrame->Frame()->pack( -fill=>'x');
+	my $titleFrame = $parentFrame->Frame() -> pack(-side => 'top', -fill => 'x');
+	$titleFrame->Label(-text => "Titre", -width => 15 )->pack( -side => 'left' );
+	$titleFrame->Label(-text => "Ceci est mon titre", -width => 15 )->pack( -side => 'left' );
+	
+	my $description = $parentFrame->Frame() -> pack(-side => 'top', -fill => 'x', -expand => 1);
+	$description->Label(-text => "Description", -width => 15 )->pack( -side => 'left' );
+	my $text = $description->Scrolled("ROText", -scrollbars => 'osoe', -height => 5 ) -> pack( -side => 'top', -fill => 'x');
+	$text->Contents("Ceci est la description\n\n\ndsdsds\nn\nddsdsds\n\ndssdsdqsdzaemcmllcds");
+	
+	my $notebook = $contentFrame->NoteBook()->pack( -fill=>'both', -expand=>1 );
+	
+	WARN "Loading sub-CR";
+	my @subCR = qw(atvcm00087450 atvcm00087451 atvcm00087452);
+	
+	foreach my $subID (@subCR) {
+		$processedCR->{subCR}->{$subID};
+		buildTab($notebook,$subID,$processedCR->{subCR}->{$subID});
+	}
+}
+
 sub buildTab {
 	my $notebook = shift;
+	my $tabName = shift;
 	my $content = shift;
 	
-	my $tab1 = $notebook->add($content->{ID}, -label => $content->{ID});
+	$content->{tabName} = $tabName;
+	
+	my $tab1 = $notebook->add($tabName, -label => $tabName);
 
-	$content{CRValues} = ;
-	$content{tab} = $tab1;
-	$content{listSubsystems} = addListBox($tab1, 'Subsystem', 'sub_system', 'Mandatory', 'Enter hereafter the subsystem',$CqFieldsDesc{sub_system}{shortDesc});
-	$content{listComponents} = addSearchableListBox($tab1, 'Component', 'component', 'Mandatory', "Select the component affected.\nIf more components are affected, please make on CR per affected component.");
-	$content{listAnalyser} = addSearchableListBox($tab1, 'Analyst', 'analyst', 'Mandatory', "Determine who will analyse the issue.", $CqFieldsDesc{analyst}{shortDesc});
-	$content{listTypes} = addListBox($tab1, 'Type', 'submitter_CR_type', 'Mandatory', "Type of modification:\n - defect for non-compliance of a requirement (specification, etc.)\n - enhancement is for various improvements (functionality, reliability, speed, etc.)", $CqFieldsDesc{submitter_CR_type}{shortDesc});
-	$content{TextProposedChanges} = addDescriptionField($tab1, 'Proposed changes', 'proposed_change','Mandatory');
+	my (@mandatoryFields, @listSubSystems);
+	$content->{tab} = $tab1;
+	$content->{CRFields} = undef;
+	$content->{listSubsystems} = addListBox($tab1, 'Subsystem', $content->{CRFields}->{'sub_system'}, 'Mandatory', 'Enter hereafter the subsystem',$CqFieldsDesc{sub_system}{shortDesc});
+	$content->{listComponents} = addSearchableListBox($tab1, 'Component', $content->{CRFields}->{'component'}, 'Mandatory', "Select the component affected.\nIf more components are affected, please make on CR per affected component.");
+	$content->{listAnalyser} = addSearchableListBox($tab1, 'Analyst', $content->{CRFields}->{'analyst'}, 'Mandatory', "Determine who will analyse the issue.", $CqFieldsDesc{analyst}{shortDesc});
+	$content->{listTypes} = addListBox($tab1, 'Type', $content->{CRFields}->{'submitter_CR_type'}, 'Mandatory', "Type of modification:\n - defect for non-compliance of a requirement (specification, etc.)\n - enhancement is for various improvements (functionality, reliability, speed, etc.)", $CqFieldsDesc{submitter_CR_type}{shortDesc});
+	$content->{TextProposedChanges} = addDescriptionField($tab1, 'Proposed changes', $content->{CRFields}->{'proposed_change'},'Mandatory');
 }
 
 exit;
