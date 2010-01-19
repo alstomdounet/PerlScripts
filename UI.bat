d3f3e5e8d2fbfc0eb6ea5a5324c97a5fd03290dc
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-01-19 14:51:36 +0000

    Added few functions.

--- UI.bat
+++ UI.bat
@@ -65,7 +65,7 @@ $mw->minsize(640,480);
 
 # SyFRSCC, Component, Type, ?headline?, CCB comment, Description / context, Proposed changes, Analysis cost+System+Hardware+Software+Validation+ Inmpacted items + Analyst + Work in progress
 #addSearchableListBox($mw, 'Selected CR', 'parent_id', undef, undef,\@listOfBugs);
-my $CrToProcess = '';
+my $CrToProcess = 'atvcm00087110';
 my $searchFrame = $mw->Frame() -> pack(-side => 'top', -fill => 'x');
 $searchFrame->Label(-text => "ID à traiter", -width => 15 )->pack( -side => 'left' );
 $searchFrame->Button(-text => "Ouvrir", -width => 15, -command => sub { loadCR($CrToProcess); } )->pack( -side => 'right' );
@@ -86,10 +86,12 @@ sub loadCR {
 	my $id = shift;
 	use Tk::ROText;
 	
-	$processedCR = undef;
-	retrieveBug($id);
+	my %fields = retrieveBug($id);
+	$processedCR = \%fields;
 	
-	use Fields;
+	open FILE,">$id.txt";
+	print FILE Dumper $processedCR;
+	close FILE;
 	
 	DEBUG "Destroying content frame" and $contentFrame->destroy() if $contentFrame;
 	
@@ -98,20 +100,18 @@ sub loadCR {
 	my $parentFrame = $contentFrame->Frame()->pack( -fill=>'x');
 	my $titleFrame = $parentFrame->Frame() -> pack(-side => 'top', -fill => 'x');
 	$titleFrame->Label(-text => "Titre", -width => 15 )->pack( -side => 'left' );
-	$titleFrame->Label(-text => "Ceci est mon titre", -width => 15 )->pack( -side => 'left' );
+	$titleFrame->Label(-text => $processedCR->{fields}{headline})->pack( -side => 'left', -fill => 'x', -expand => 1 );
 	
 	my $description = $parentFrame->Frame() -> pack(-side => 'top', -fill => 'x', -expand => 1);
 	$description->Label(-text => "Description", -width => 15 )->pack( -side => 'left' );
 	my $text = $description->Scrolled("ROText", -scrollbars => 'osoe', -height => 5 ) -> pack( -side => 'top', -fill => 'x');
-	$text->Contents("Ceci est la description\n\n\ndsdsds\nn\nddsdsds\n\ndssdsdqsdzaemcmllcds");
+	$text->Contents($processedCR->{fields}{description});
 	
 	my $notebook = $contentFrame->NoteBook()->pack( -fill=>'both', -expand=>1 );
 	
-	WARN "Loading sub-CR";
-	my @subCR = qw(atvcm00087450 atvcm00087451 atvcm00087452);
-	
-	foreach my $subID (@subCR) {
-		buildTab($notebook,$subID,$processedCR->{subCR}->{$subID});
+	foreach my $subID (sort keys %{$processedCR->{childs}}) {
+		DEBUG "Processing child $subID";
+		buildTab($notebook,$subID,$processedCR->{childs}{$subID});
 	}
 }
 
@@ -126,16 +126,23 @@ sub buildTab {
 
 	my (@mandatoryFields, @listSubSystems);
 	$content->{tab} = $tab1;
-	$content->{CRFields} = undef;
-	$content->{listSubsystems} = addListBox($tab1, 'Subsystem', $content->{CRFields}->{sub_system}, 'Mandatory', 'Enter hereafter the subsystem',$CqFieldsDesc{sub_system}{shortDesc});
-	$content->{listComponents} = addSearchableListBox($tab1, 'Component', $content->{CRFields}->{component}, 'Mandatory', "Select the component affected.\nIf more components are affected, please make on CR per affected component.");
-	$content->{listAnalyser} = addSearchableListBox($tab1, 'Analyst', $content->{CRFields}->{analyst}, 'Mandatory', "Determine who will analyse the issue.", $CqFieldsDesc{analyst}{shortDesc});
-	$content->{listTypes} = addListBox($tab1, 'Type', $content->{CRFields}->{submitter_CR_type}, 'Mandatory', "Type of modification:\n - defect for non-compliance of a requirement (specification, etc.)\n - enhancement is for various improvements (functionality, reliability, speed, etc.)", $CqFieldsDesc{submitter_CR_type}{shortDesc});
-	$content->{TextProposedChanges} = addDescriptionField($tab1, 'Proposed changes', $content->{CRFields}->{proposed_change},'Mandatory');
+	my @test;
+	foreach my $item (@{$CqFieldsDesc{sub_system}{shortDesc}}) {
+		push (@test, { -name => $item, -value => $CqFieldsDesc{sub_system}{equivTable}{$item} });
+		#push (@test, { -name => $item, -value => $item.$item });
+	}
+	$content->{listSubsystems} = addListBox($tab1, 'Subsystem', \@test, \$content->{fields}{sub_system});
+	my @list;
+	#$content->{listComponents} = addListBox($tab1, 'Component', \@list, \$content->{fields}{component});
+	$content->{listAnalyser} = addListBox($tab1, 'Analyst', $CqFieldsDesc{analyst}{shortDesc}, \$content->{fields}{analyst}, -searchable => 1);
+	$content->{listTypes} = addListBox($tab1, 'Type', $CqFieldsDesc{submitter_CR_type}{shortDesc}, \$content->{fields}{submitter_CR_type});
+	$content->{TextProposedChanges} = addDescriptionField($tab1, 'Proposed changes', $content->{fields}{proposed_change});
 }
 
 sub analyseListboxes {	
-	# my $backup = $bugDescription{component};
+	#my ($value_subsystem, $item) = @_;
+	
+	#my $backup = $bugDescription{component};
 	# if($bugDescription{sub_system} and $bugDescription{sub_system} ne $lastSelectedSubsystem) {
 		# if($CqFieldsDesc{component}{commentTable}{$bugDescription{sub_system}}) {
 			# changeList($listComponents, $CqFieldsDesc{component}{commentTable}{$bugDescription{sub_system}}, $backup);
@@ -153,6 +160,13 @@ sub retrieveBug {
 	my %bug;
 	use ClearquestMgt qw(connectCQ disconnectCQ getChangeRequestFields getEntity editEntity getEntityFields getChilds getAvailableActions getFieldsRequiredness cancelAction); 
 
+	my $idDatabase = $bugID.'.db';
+	if (-r $idDatabase) {
+		WARN "Using Database unstead of direct copy";
+		my $bug = retrieve($idDatabase);
+		return %$bug;
+	}
+	
 	INFO "Connecting to Clearquest";
 	connectCQ ('gmanciet', 'jsne1983aS!', 'atvcm');
 	INFO "Retrieving needed informations";
@@ -176,7 +190,7 @@ sub retrieveBug {
 		}
 		$bug{childs}{$childID} = \%child;
 	}
-	store(\%bug, $bugID.'.db');
+	store(\%bug, $idDatabase);
 	return %bug;
 }
 
