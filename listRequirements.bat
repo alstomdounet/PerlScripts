cf8a62a11591423e374099f990df80030c9ac72d
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-05-07 06:58:52 +0000

    Improved coding by creating joinRequirements to factorize code.

--- listRequirements.bat
+++ listRequirements.bat
@@ -25,6 +25,7 @@ use open ':encoding(utf8)';
 use constant {
 	PROGRAM_VERSION => '0.2',
 	TEMPLATE_DIRECTORY => './Templates/',
+	DEBUG_MODE => 0,
 };
 
 INFO "Starting program (V ".PROGRAM_VERSION.")";
@@ -35,82 +36,54 @@ my $SCRIPT_DIRECTORY = getScriptDirectory();
 my $DATA_DIRECTORY = $SCRIPT_DIRECTORY."Data\\";
 DEBUG "Using $DATA_DIRECTORY as script directory";
 
-INFO "Reading source files";
+INFO "Extracting required informatiosn from files";
 my %source;
 my %fields;
 
-%fields = ('Exigence_CDC' => 0, 'Texte' => 1);
-$source{CDC_LIST} = loadExcel($config->{documents}->{ClauseByClause}->{FileName}, $config->{documents}->{ClauseByClause}->{Sheet}, \%fields);
+INFO "generating list of requirements compliant for REI side";
 
-%fields = ('Exigence_VBN' => 0, 'Texte' => 1);
-$source{VBN_LIST} = loadExcel($config->{documents}->{Requirements_VBN}->{FileName}, $config->{documents}->{Requirements_VBN}->{Sheet}, \%fields);
+# my %list_CDC1 = ( 'REQ-CDC-01' => 1, 'REQ-CDC-02' => 2, 'REQ-CDC-03' => 3, 'REQ-CDC-04' => 4);
+# my %list_REI1 = ( 'REQ-REI-01' => 1, 'REQ-REI-02' => 2, 'REQ-REI-03' => 3, 'REQ-REI-04' => 4);
+# my @list3;
+# push(@list3, {'Exigence_REI' => 'REQ-REI-12', 'Exigence_CDC' => 'REQ-CDC-02', __LineNumber => 3});
+# push(@list3, {'Exigence_REI' => 'REQ-REI-01', 'Exigence_CDC' => 'REQ-CDC-04', __LineNumber => 6});
+# push(@list3, {'Exigence_REI' => 'REQ-REI-01', 'Exigence_CDC' => 'REQ-CDC-12', __LineNumber => 9});
 
-%fields = ('Exigence_REI' => 0, 'Texte' => 1);
-$source{REI_LIST} = loadExcel($config->{documents}->{Requirements_REI}->{FileName}, $config->{documents}->{Requirements_REI}->{Sheet}, \%fields);
+# my %list_VBN_CDC1 = joinRequirements(\%list_CDC1, \%list_REI1, \@list3, 'Exigence_CDC', 'Exigence_REI');
+# exit;
 
-%fields = ('Exigence_CDC' => 0, 'Exigence_VBN' => 1, 'state' => 2, 'req_level' => 3, 'Risk' => 4);
-$source{VBN_CDC_List} = loadExcel($config->{documents}->{Requirements_VBN_CBC}->{FileName}, $config->{documents}->{Requirements_VBN_CBC}->{Sheet}, \%fields);
+if (not DEBUG_MODE or not -r "Requirements_image.db") {
+	%fields = ('Exigence_CDC' => 0, 'Texte' => 1);
+	$source{CDC_LIST} = loadExcel($config->{documents}->{ClauseByClause}->{FileName}, $config->{documents}->{ClauseByClause}->{Sheet}, \%fields);
 
-%fields = ('Exigence_CDC' => 0, 'Exigence_REI' => 1, 'Applicabilite' => 2, 'Risk' => 3, 'Comment' => 4);
-$source{REI_CDC_List} = loadExcel($config->{documents}->{Requirements_REI_CBC}->{FileName}, $config->{documents}->{Requirements_REI_CBC}->{Sheet}, \%fields);
+	%fields = ('Exigence_VBN' => 0, 'Texte' => 1);
+	$source{VBN_LIST} = loadExcel($config->{documents}->{Requirements_VBN}->{FileName}, $config->{documents}->{Requirements_VBN}->{Sheet}, \%fields);
+
+	%fields = ('Exigence_REI' => 0, 'Texte' => 1);
+	$source{REI_LIST} = loadExcel($config->{documents}->{Requirements_REI}->{FileName}, $config->{documents}->{Requirements_REI}->{Sheet}, \%fields);
+
+	%fields = ('Exigence_CDC' => 0, 'Exigence_VBN' => 1, 'state' => 2, 'req_level' => 3, 'Risk' => 4);
+	$source{VBN_CDC_List} = loadExcel($config->{documents}->{Requirements_VBN_CBC}->{FileName}, $config->{documents}->{Requirements_VBN_CBC}->{Sheet}, \%fields);
+
+	%fields = ('Exigence_CDC' => 0, 'Exigence_REI' => 1, 'Applicabilite' => 2, 'Risk' => 3, 'Comment' => 4);
+	$source{REI_CDC_List} = loadExcel($config->{documents}->{Requirements_REI_CBC}->{FileName}, $config->{documents}->{Requirements_REI_CBC}->{Sheet}, \%fields);
+	store(\%source, "Requirements_image.db");
+}
+else {
+	%source = %{retrieve("Requirements_image.db")};
+}
 
 INFO "Preprocessing source files";
+WARN "Missing analysis for source requirements";
 my %list_CDC = map { $_->{Exigence_CDC} => $_ } @{$source{CDC_LIST}};
 my %list_VBN = map { $_->{Exigence_VBN} => $_ } @{$source{VBN_LIST}};
 my %list_REI = map { $_->{Exigence_REI} => $_ } @{$source{REI_LIST}};
 
-
-
 INFO "generating list of requirements compliant for VBN side";
-my %list_VBN_CDC;
-my $i = 1;
-foreach (sort @{$source{VBN_CDC_List}}) {
-	$i++;
-	#ERROR "Line $i: No equivalent found for VBN key called \"$_->{Exigence_VBN}\"" and next unless $list_VBN{$_->{Exigence_VBN}};
-	#ERROR "Line $i: No equivalent found for CDC key called \"$_->{Exigence_CDC}\"" and next unless $list_CDC{$_->{Exigence_CDC}};
-	my %item;
-	my $orig_item = $list_VBN{$_->{Exigence_VBN}};
-	$item{Texte} = $orig_item->{Texte};
-	if(defined $_->{Risk} and "$_->{Risk}" ne "") {
-		$item{Risk} = "R".$_->{Risk};
-	}
-	
-	$item{Req_ID} = $_->{Exigence_VBN};
-	push(@{$list_VBN_CDC{$_->{Exigence_CDC}}}, \%item);
-}
+my ($list_REI_CDC, $errors_REI_CDC) = joinRequirements(\%list_CDC, \%list_REI, $source{REI_CDC_List}, 'Exigence_CDC', 'Exigence_REI');
 
 INFO "generating list of requirements compliant for REI side";
-my %list_REI_CDC;
-my %errors_reported;
-$i = 1;
-foreach (sort @{$source{REI_CDC_List}}) {
-	$i++;
-
-	unless ($list_REI{$_->{Exigence_REI}}) {
-		next if $errors_reported{$_->{Exigence_REI}};
-		
-		ERROR "Line $_->{__LineNumber}: No equivalent found for REI key called \"$_->{Exigence_REI}\"";
-		$errors_reported{$_->{Exigence_REI}}++;
-		next;
-	}
-	
-	unless ($list_CDC{$_->{Exigence_CDC}}) {
-		next if $errors_reported{$_->{Exigence_CDC}};
-		ERROR "Line $_->{__LineNumber}: No equivalent found for CDC key called \"$_->{Exigence_CDC}\"";
-		$errors_reported{$_->{Exigence_CDC}}++;
-		next;
-	}
-	
-	my %item;
-	if( defined $_->{Risk} and "$_->{Risk}" ne "") {
-		$item{Risk} = "R".$_->{Risk};
-	}
-	
-	my $orig_item = $list_REI{$_->{Exigence_REI}};
-	$item{Texte} = $orig_item->{Texte};
-	$item{Req_ID} = $_->{Exigence_REI};
-	push(@{$list_REI_CDC{$_->{Exigence_CDC}}}, \%item);
-}
+my ($list_VBN_CDC, $errors_VBN_CDC) = joinRequirements(\%list_CDC, \%list_VBN, $source{VBN_CDC_List}, 'Exigence_CDC', 'Exigence_VBN');
 
 INFO "Generating final mapping";
 my @final_report;
@@ -119,8 +92,8 @@ foreach (sort @{$source{CDC_LIST}}) {
 	my %requirement;
 	$requirement{Texte} = $_->{Texte};
 	$requirement{Req_ID} = $reference;
-	$requirement{REQUIREMENTS_VBN} = $list_VBN_CDC{$reference} if $list_VBN_CDC{$reference};
-	$requirement{REQUIREMENTS_REI} = $list_REI_CDC{$reference} if $list_REI_CDC{$reference};
+	$requirement{REQUIREMENTS_VBN} = $list_VBN_CDC->{$reference} if $list_VBN_CDC->{$reference};
+	$requirement{REQUIREMENTS_REI} = $list_REI_CDC->{$reference} if $list_REI_CDC->{$reference};
 
 	push(@final_report, \%requirement);
 }
@@ -136,6 +109,59 @@ $t->param(DATE => $tm);
 
 print FILE $t->output;
 close(FILE);
+
+sub joinRequirements {
+	my($list_reference, $list_to_link, $list_links, $key_ref, $key_link) = @_;
+
+	DEBUG "Processing list of requirements";
+	
+	my %list;
+	my %errors;
+	my %analysis_table;
+	
+	foreach (sort @{$list_links}) {
+		# Identifying referenced 
+		$analysis_table{LIST_REF}->{$_->{$key_ref}}++;
+		$analysis_table{LIST_TO_LINK}->{$_->{$key_link}}++;
+
+		unless ($list_to_link->{$_->{$key_link}}) {
+			next if $errors{TO_LINK_UNMATCHED}->{$_->{$key_link}};
+			
+			DEBUG "Line $_->{__LineNumber}: No equivalent found for LINKS key called \"$_->{$key_link}\"";
+			$errors{TO_LINK_UNMATCHED}->{$_->{$key_link}} = $_->{__LineNumber};
+			next;
+		}
+		
+		unless ($list_reference->{$_->{$key_ref}}) {
+			next if $errors{REF_UNMATCHED}->{$_->{$key_ref}};
+			
+			DEBUG "Line $_->{__LineNumber}: No equivalent found for REFERENCES key called \"$_->{$key_ref}\"";
+			$errors{REF_UNMATCHED}->{$_->{$key_ref}} = $_->{__LineNumber};
+			next;
+		}
+		
+		my %item;
+		my $orig_item = $list_to_link->{$_->{$key_link}};
+		$item{Texte} = $orig_item->{Texte};
+		if(defined $_->{Risk} and "$_->{Risk}" ne "") {
+			$item{Risk} = "R".$_->{Risk};
+		}
+		
+		$item{Req_ID} = $_->{$key_link};
+		push(@{$list{$_->{$key_ref}}}, \%item);
+	}
+	
+	foreach my $ref (keys %$list_reference) {
+		$errors{REF_UNUSED}{$ref}++ unless $analysis_table{LIST_REF}{$ref};
+	}
+	
+	foreach my $ref (keys %$list_to_link) {
+		$errors{TO_LINK_UNUSED}{$ref}++ unless $analysis_table{LIST_TO_LINK}->{$ref};
+	}
+	
+	return (\%list, \%errors);
+	
+}
 
 sub loadExcel {
 	my ($filename, $sheet, $header) = @_;
