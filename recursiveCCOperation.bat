6756020f7a6da31c8f5995921359f719297fd787
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2010-06-11 10:21:58 +0000

    Added behavior to recursively add files.

--- recursiveCCOperation.bat
+++ recursiveCCOperation.bat
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 use Common;
 use GraphicalCommon;
-use ClearcaseMgt qw(getLabelListVOB setLabel renameElement checkoutElement getAttribute setAttribute isCheckedoutElement isPrivateElement uncheckoutElement checkinElement isLatest);
+use ClearcaseMgt qw(addToSource checkoutElement isCheckedoutElement isPrivateElement uncheckoutElement checkinElement);
 use Data::Dumper;
 use File::Basename;
 use File::Copy;
@@ -37,36 +37,58 @@ INFO "Starting program (".PROGRAM_VERSION.")";
 
 my $currentOperation = $ARGV[0];
 my $currentDirectory = $ARGV[1];
-LOGDIE "Program needs two argument. first is operation among \"-rec_add\", \"-rec_ci\", \"-rec_co\" followed by a valid directory" unless $currentDirectory and $currentOperation;
+my $comment = $ARGV[2];
+LOGDIE "Program needs three argument. first is operation among \"-rec_add\", \"-rec_ci\", \"-rec_co\" followed by a valid directory and finally a good comment." unless $currentDirectory and $currentOperation and $comment;
 
-LOGDIE "Argument has to be a valid operation among \"-rec_add\", \"-rec_ci\", \"-rec_co\", and you have entered \"$currentOperation\"" unless $currentOperation eq OPERATION_RECURSIVE_CHECKIN or $currentOperation eq OPERATION_RECURSIVE_CHECKOUT or $currentOperation eq OPERATION_RECURSIVE_ADD;
+LOGDIE "Argument #1 has to be a valid operation among \"-rec_add\", \"-rec_ci\", \"-rec_co\", and you have entered \"$currentOperation\"" unless $currentOperation eq OPERATION_RECURSIVE_CHECKIN or $currentOperation eq OPERATION_RECURSIVE_CHECKOUT or $currentOperation eq OPERATION_RECURSIVE_ADD;
+LOGDIE "Argument #2 has to be an existing and valid directory. Argument is actually \"$currentDirectory\"" unless -e $currentDirectory and -r $currentDirectory;
+LOGDIE "Argument #3 has to be a comment. Argument is actually \"$comment\"" unless $comment;
 
-
-LOGDIE "Argument used has to be an existing and valid directory. Argument is actually \"$currentDirectory\"" unless -e $currentDirectory and -r $currentDirectory;
+chdir($currentDirectory);
+chdir("..");
+DEBUG "Root directory needs to be checked out." and checkoutElement(".",$comment) unless isCheckedoutElement(".");
+chdir($currentDirectory);
+conditionalAddToSource($currentDirectory, $comment);
 
 read_directory($currentDirectory);
 
 sub read_directory {
 	my ($directory) = @_;
 	
+	chdir($directory);
 	my $currentDirectory = cwd();
 	DEBUG "Opening directory \"$currentDirectory\"";
-	opendir(IMD, $directory) || LOGDIE("Cannot open directory \"$directory\"");
-	LOGDIE "Root directory \"$currentDirectory\" is not in SCM" if isPrivateElement($directory);
+	opendir(IMD, ".") || LOGDIE("Cannot open current directory");
+	LOGDIE "Root directory \"$currentDirectory\" is not in SCM" if isPrivateElement(".");
+	DEBUG "Root directory needs to be checked out." and checkoutElement(".",$comment) unless isCheckedoutElement(".");
 	
 	my @files = sort readdir(IMD);
 	closedir(IMD);
-	chdir($directory);
-	foreach my $file (@files) {
-		next if $file =~/^\.{1,2}$/;
-		if(-d $file) {
-			read_directory($file);
-		}
-		else {
-			DEBUG "FILE ".$file;
+
+	foreach my $element (@files) {
+		next if $element =~/^\.{1,2}$/;
+		
+		conditionalAddToSource($element, $comment);
+		
+		if(-d $element) {
+			read_directory($element);
 		}
 	} 
 	chdir("..");
+ }
+ 
+ sub conditionalAddToSource {
+	my ($element, $comment) = @_;
+ 
+	if (isPrivateElement($element)) {
+		DEBUG "File \"$element\" needs to be added";
+		if(addToSource($element, $comment)) {
+			DEBUG "Element \"$element\" was added to source control";
+		}
+		else {
+			ERROR "Element \"$element\" had a problem";
+		}
+	}
  }
 
  exit;
